89113763afcc09a8613e1f648bb0a0acb34de2a5
ruzko <75946143+ruzko@users.noreply.github.com>
2022-05-11 22:34:51 +0200

    time-based flipping of input values, clean up code

--- main.cpp
+++ main.cpp
@@ -6,37 +6,42 @@
  * 3. write '1' to 'outputFile' if pin state is 'HIGH' and write '0' to 'outputFile' if pin state is 'LOW',
  * 4. continue running until user says otherwise.
  *
- * Each step is divided into its own code block, containing logic and exception handling.
- * As a testing environment, step 2 is replaced by a code block which emulates pin state by changing between 'LOW' and 'HIGH' every 5 seconds
- *
+Each step is divided into its own code block, containing logic and exception handling.
+As a testing environment, step 2 is replaced by a code block which emulates pin state by changing between 'LOW' and 'HIGH' every 5 seconds
  */
-#include <stdio.h>
+
 //#include <pigpio.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <filesystem>
 #include <ctime>
-#include <vector>
-#include <tuple>
+
 
 using namespace std;
-//using std::fstream;
 using std::cout;
 using std::cerr;
 using std::endl;
 using std::string;
 using std::fstream;
 
-//using std::filesystem::remove
 
+//global declarations
 const char* outputFile ="triggerOutStatus.txt";
 const char* readPinFile = "dummy_gpio.txt";
 char currentStatus;  //A store for the single integers read from 'readPinFile'
+fstream ifs;
+fstream ofs;
+int interval = 5; //seconds between swapping of 'inputFile'
 
-//int main(int argc, char **argv) {
+void function();
+
 int main () {
 
+// Starting the count
+    time_t startTime, end;
+    startTime = time(0);
+    //deltaTime = clock();
 /*  if (gpioInitialise() < 0)
   {
     return 1;
@@ -75,7 +80,7 @@ fstream ifs; //(readPinFile); //declaring a file stream object
 ifs.close();
 
 
-ofstream ofs;//(outputFile);//declaring a file stream object
+ofstream ofs;//declaring a file stream object
 //Testing whether the file can be read from, and
   ofs.open(outputFile, ios::out);
   if (ofs.is_open())
@@ -89,33 +94,44 @@ ofstream ofs;//(outputFile);//declaring a file stream object
   }
 ofs.close();
 
+ while (1)
+  {
+
+    if (time(0) - startTime == interval)
+    {
+      function();
+      startTime=startTime+interval;
+    }
+  }
+   return 0;
+}
 // End of code block 1
+
+//------------------------------------------------------------//
 
 //Start of temporary code block 2, swapping state of 'pin' between 'LOW' and 'HIGH' every 5 seconds. First deleting readPinFile
 
-
-// int currentStatus;
-//while (ifs.get(currentStatus)) //read
-//{
-ifs.open(readPinFile, ios::in);
+void function()
+{
+  //opening 'readPinFile' as read-only filestream, reading the first character and storing it in 'currentStatus'
+  ifs.open(readPinFile, ios::in);
   ifs.get(currentStatus);
 
+  //printing 'currentStatus' for testing purposes
   cout << "input is currently " << currentStatus << endl;
 
-//  }
+  //closing read-only filestream of 'readPinFile'
   ifs.close();
-  int runtime = '0';
-  int interval = '5'; //milliseconds
- // while (runtime < 2000)
-//  {
-/*      int deltaTime;
-      int clock = clock_t();
-      deltaTime = clock - runtime;
-    if (deltaTime == interval)
-*///    {
+
+  //declaring variables for the sake of timing the swapping of values written to 'readPinFile'
+//  int runtime = '0';
+//  ; //milliseconds
+
+    {
+// Opening 'readPinFile' as writable filestream
        ifs.open(readPinFile, ios::out);
 
-//    }
+// reading value stored in 'currentStatus', and writing the opposite value to 'readPinFile'. In effect flipping the value each permutation
         if (currentStatus == '1')
         {
             ifs.put('0');
@@ -125,16 +141,13 @@ ifs.open(readPinFile, ios::in);
         {
             ifs.put('1');
             ifs.close();
-       }
+        }
 
+    }
 
- //   ifs.put('0');
- //   ifs.close();
-    runtime=runtime+interval;
-
-//  }
-//}
 //End of temporary code block 2
+
+//--------------------------------------------------------------------//
 
 //Start of actual code block 2, reading status of pin into a variable
 /*
@@ -146,31 +159,41 @@ ifs.open(readPinFile, ios::in);
 
 //End of actual code block 2
 
+//--------------------------------------------------------------------//
+
 //Start of code block 3, writing currentStatus to outputFile
 
+//declaring variable 'outputStatus' for storing integers
 int outputStatus;
+
+//opening 'outputFile' as a writable filestream
 ofs.open(outputFile, ios::out);
+
+// A switch case which reads the value stored in 'currentStatus' and writes the same value to 'outputFile'
   switch(currentStatus)
   {
+    // if currentStatus equals 1, it means the extruder is active. Possible to trigger a recording
     case '1' :
-    fprintf(stdout, "<PRESSED>\n");
+    cout << "<Extruder active>" << endl;
     outputStatus = '1';
     ofs.put(outputStatus);
     ofs.close();
     break;
 
+    //if currentStatus equals 0, it means the extruder is inactive. Don't trigger a recording
     case '0' :
-    fprintf(stdout, "<NONE>\n");
+    cout << "<Not extruding>" << endl;
     outputStatus = '0';
     ofs.put(outputStatus);
     ofs.close();
     break;
 
+    // Error handling in case something goes wrong
     default :
       cout << "Switch statement failed" << endl;
 
   }
 
 //  gpioTerminate(); // Compulsory. If gpio isn't terminated, pins aren't uninitialized and can cause trouble for later assignments.
-  return 0;
+
 }
